8b4341dd7bb8fedb4196d52c24ae1238a83624a4
Juan Gallostra <juangallostra@gmail.com>
2020-11-19 09:25:50 +0100

    Fixes and improvements

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import json
 import time
 
+COUNTRY_KEY = 'country'
+COUNTRY_CODE_KEY = 'country_code'
+ADDRESS_KEY = 'address'
+NAME_KEY = 'name'
+
 def monkey_patch_geopy_base(instance, lang='en'):
     instance.headers['Accept-Language'] = lang
 
@@ -10,25 +15,31 @@ def monkey_patch_geopy_base(instance, lang='en'):
 def main(db_path='../BetaLibrary/', is_update=True):
     # walk all data
     areas = next(os.walk(db_path + 'data/zones/'))[1]
+    country_dict = {}
     for area in areas:
         time.sleep(1) # rate limiter (as per Geopy's documentation)
         # Load zone data
         datafile = db_path + 'data/zones/' + area + '/' + area + '.txt'
         with open(datafile, encoding='utf-8') as data:
             area_data = json.load(data)
-        if is_update and area_data.get('country', ''):
-            print(f'Skipping update for {area_data["name"]}')
+        if is_update and area_data.get(COUNTRY_KEY, ''):
+            print(f'Skipping update for {area_data[NAME_KEY]}')
             continue
         geolocator = Nominatim(user_agent='my_email@myserver.com')
         # Patch geopy to return countries in English
         monkey_patch_geopy_base(geolocator, lang='en')
         location = geolocator.reverse(f"{area_data['latitude']}, {area_data['longitude']}")
-        country_code = location.raw['address']['country_code']
+        try:
+            country = location.raw[ADDRESS_KEY][COUNTRY_KEY]
+            # store match for future lookups if a country is missing in some zone data
+            country_dict[location.raw[ADDRESS_KEY][COUNTRY_CODE_KEY]] = location.raw[ADDRESS_KEY][COUNTRY_KEY]
+        except: 
+            country = country_dict[location.raw[ADDRESS_KEY][COUNTRY_CODE_KEY]]
         # print(location.raw)
-        print(area_data['name'], country_code)
+        print(area_data[COUNTRY_KEY], country)
         # print(location.address)
 
-        area_data['country'] = country_code
+        area_data[COUNTRY_KEY] = country
         with open(datafile, 'w', encoding='utf-8') as data:
            json.dump(area_data, data, indent=4)
 
